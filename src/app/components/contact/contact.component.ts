e2165bc227aa3afd47780d579c4a4ce1301121ff
Udaya Anushanka <udaya_a@epiclanka.net>
2026-02-13 15:15:29 +0530

    Added integrations for the email sending

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
@@ -12,4 +14,38 @@ export class ContactComponent {
   email = 'beligahamullage@gmail.com';
   phone = '+94 77 563 2256';
   location = '136, Mulana, Narawala, Poddala, Galle, Sri Lanka';
+
+  name = '';
+  userEmail = '';
+  message = '';
+
+  sendMessage() {
+    const now = new Date();
+
+    const formattedTime = now.toLocaleString('en-LK', {
+      timeZone: 'Asia/Colombo',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+
+    const templateParams = {
+      from_name: this.name,
+      from_email: this.userEmail,
+      message: this.message,
+      time: formattedTime,
+    };
+
+    emailjs
+      .send('service_2xi2vzu', 'template_jj7up4q', templateParams, 'kujMxXbwYBrBuqUss')
+      .then(() => {
+        alert('Message sent successfully!');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
